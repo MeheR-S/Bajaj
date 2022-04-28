625efea17d9d7a7091942af12058162744d293d6
MeheR S <meherashekar@gmail.com>
2022-04-28 21:37:34 +0530

    upload BAJAJ

---
+++ src/main/java/com/app/dto/Request.java
@@ -0,0 +1,40 @@
+package com.app.dto;
+
+import java.util.Arrays;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Request {
+	//"data": [“A”,”1”,”334”,”4”,”R”]
+	
+	private String[]  alphabets;
+
+	public Request() {
+		super();
+	}
+
+	public Request(String[] alphabets) {
+		super();
+		this.alphabets = alphabets;
+	}
+
+	public String[] getAlphabets() {
+		return alphabets;
+	}
+
+	public void setAlphabets(String[] alphabets) {
+		this.alphabets = alphabets;
+	}
+
+	@Override
+	public String toString() {
+		return "Request [alphabets=" + Arrays.toString(alphabets) + "]";
+	}
+	
+	
+}
